a49206a1e449316e7eb3f6d43d5e37522d5f4f9f
nainaarijo <nainaarijo123@gmail.com>
2024-09-18 23:57:32 +0500

    final

---
+++ script/sign-in.js
@@ -0,0 +1,37 @@
+let signInForm = document.getElementById('sign-in')
+signInForm.addEventListener('submit', async function(e){
+    e.preventDefault();
+
+let userInputEmail = document.getElementById('email').value.trim()
+let userInputPassword = document.getElementById('password').value.trim()    
+let localUsers = localStorage.getItem('user')    
+let users = JSON.parse(localUsers)
+
+
+try {
+    let userData = await fetch('https://fakestoreapi.com/users');
+    let usersInfo = await userData.json()
+
+
+    const inputValue = usersInfo.find(
+        user => user.email === userInputEmail && user.password  === userInputPassword);
+
+
+        if(inputValue){
+             window.location.href = "/index.html";
+        }else  {
+            users.forEach(user => {
+            if (user.userEmail === userInputEmail && user.userPassword === userInputPassword){
+                window.location.href = "/index.html"
+            }else{
+                alert("Check Email & Password")
+            }
+            });
+
+        }
+
+        
+} catch(error){
+    console.log("error", error);
+}
+})
---
+++ script/sign-up.js
@@ -0,0 +1,19 @@
+let signUpForm = document.getElementById('sign-up')
+signUpForm.addEventListener('submit',function(event){
+event.preventDefault()
+
+let email = document.getElementById('email').value.trim();
+let password = document.getElementById('password').value.trim();
+let arrayUsers = []
+if(email && password){
+    let objUsers = {
+        userEmail: email,
+        userPassword: password
+    }
+
+    arrayUsers.push(objUsers)
+    let userDetails = JSON.stringify(arrayUsers)
+    localStorage.setItem('user',userDetails)
+    window.location.href = "/pages/sign-up.html"
+}
+})
